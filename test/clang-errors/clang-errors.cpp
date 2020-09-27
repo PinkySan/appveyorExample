4b9adb385abcdf4730a40171edc9b8ef5880207d
Benedict Hartmann <bene.hartmann@web.de>
2020-09-27 18:20:06 +0200

    moving definition into cpp and fixing errors

---
+++ test/clang-errors/clang-errors.cpp
@@ -0,0 +1,24 @@
+#include "clang-errors.h"
+
+int misc_string_compare()
+{
+    std::string str1{ "a" };
+    std::string str2{ "b" };
+
+    return str1.compare(str2);
+}
+
+void misc_string_integer_assignment()
+{
+    std::string s;
+    int x = 5965;
+    s = 6;
+    s = x;
+}
+
+void misc_uniqueptr_reset_release()
+{
+    struct Foo;
+    std::unique_ptr<Foo> x, y;
+    x.reset(y.release());
+}
--- test/clang-errors/clang-errors.h
+++ test/clang-errors/clang-errors.h
@@ -1,24 +1,8 @@
 #include <string>
 #include <memory>
 
-int misc_string_compare()
-{
-    std::string str1{ "a" };
-    std::string str2{ "b" };
+int misc_string_compare();
 
-    return str1.compare(str2));
-}
+void misc_string_integer_assignment();
 
-void misc_string_integer_assignment()
-{
-    std::string s;
-    int x = 5965;
-    s = 6;
-    s = x;
-}
-
-void misc_uniqueptr_reset_release()
-{
-    std::unique_ptr<Foo> x, y;
-    x.reset(y.release());->x = std::move(y);
-}
+void misc_uniqueptr_reset_release();
